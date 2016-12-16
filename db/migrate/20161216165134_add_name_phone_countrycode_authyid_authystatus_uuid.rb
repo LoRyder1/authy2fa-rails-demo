e6e65d64b150b1f317d17ea4973c8067220f2901
Lovinder Pnag <lspnag@gmail.com>
2016-12-16 08:57:09 -0800

    unicorn.rb and new columns

---
+++ db/migrate/20161216165134_add_name_phone_countrycode_authyid_authystatus_uuid.rb
@@ -0,0 +1,10 @@
+class AddNamePhoneCountrycodeAuthyidAuthystatusUuid < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :country_code, :string
+    add_column :users, :authy_id, :string
+    add_column :users, :authy_status, :integer, :null => false, :default => 0
+    add_column :users, :uuid, :string
+  end
+end
