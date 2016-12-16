6434a63b1d862919332e94bf9705434ff4da990b
Lovinder Pnag <lspnag@gmail.com>
2016-12-16 09:00:02 -0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  
+  # Account section
+  resource :account, only: [:show]
+
+  # Sessions
+  resources :sessions, only: [:new, :create]
+  get "sessions/destroy"
+  get "sessions/two_factor"
+
+  post "authy/callback" => 'authy#callback'
+  get "authy/status" => 'authy#one_touch_status'
+  post "authy/send_token"
+  post "authy/verify"
+
+  # Create users
+  resources :users, only: [:new, :create]
+
+  # Home page
+  root 'main#index'
 end
